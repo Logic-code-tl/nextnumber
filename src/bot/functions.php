430834d6601735b72e9d4d583a8880aead3a2e7d
sadegh <sadeghmt9@gmail.com>
2023-07-21 20:22:07 +0330

    add more functions

--- src/bot/functions.php
+++ src/bot/functions.php
@@ -1,13 +1,13 @@
 <?php
 
-
+declare(strict_types=1);
 
 function bot($method, $params)
 {
 
-    return file_get_contents(API_ENDPOINT . $method . '?' . http_build_query($params));
+    file_get_contents(API_ENDPOINT . $method . '?' . http_build_query($params));
 }
-function create_payment($id, $amount)
+function create_payment(int $id,  int $amount)
 {
 
     return PAYMENT_CREATE_URL . '?' . 'id=' . $id . '&' . 'amount=' . $amount;
@@ -26,24 +26,37 @@ function getRandomString($n)
 }
 
 
-function send_message(string $text, int $chat_id = null, array $reply_markup = null, string $parse_mode = 'html', array $param = null)
+function send_message(string $text = null, int $chat_id = null, array $reply_markup = null, string $parse_mode = 'html', array $param = null)
 {
     global $update;
-    $chat_id ?? $chat_id = $update->message->chat->id;
 
 
 
     if ($param == null) {
-        $params = [
-            'text' => $text,
+        if ($reply_markup != null) {
+            $reply_markup = json_encode($reply_markup);
+        }
+
+        $chat_id ?? $chat_id = $update->message->chat->id ?? $chat_id = $update->callback_query->from->id;
+
+        $param = [
             'chat_id' => $chat_id,
+            'text' => $text,
             'parse_mode' => $parse_mode,
-            'reply_markup' => json_encode($reply_markup)
+            'reply_markup' => $reply_markup
 
         ];
-        return file_get_contents(API_ENDPOINT . 'sendMessage?' . http_build_query($params));
+
+        return file_get_contents(API_ENDPOINT . 'sendMessage?' . http_build_query($param));
     } else {
 
         return file_get_contents(API_ENDPOINT . 'sendMessage?' . http_build_query($param));
     }
+}
+function validate_data($data, $token)
+{
+    $hash = $data['hash'];
+    $data_check_string = "auth_date=$data[auth_date]\nquery_id=$data[query_id]\nuser=$data[user]";
+    $secret_key = hash_hmac('sha256', $token, "WebAppData", true);
+    return hash_hmac('sha256', $data_check_string, $secret_key) == $hash; // true if data is from Telegram
 }
